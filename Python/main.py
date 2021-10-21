17464788367c6611e011e942d0bf83b6f897b9be
Megan Mattina <petersen.megan9646@gmail.com>
2021-10-21 07:30:19 -0600

    Set up Python at Home as well

--- Python/main.py
+++ Python/main.py
@@ -2,6 +2,8 @@ import maya.cmds as cmds
 
 cmds.polySphere()
 
+
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
