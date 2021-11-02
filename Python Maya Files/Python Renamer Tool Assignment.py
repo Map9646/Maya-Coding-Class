1dfba1b7884ce9c2e426df3c91125b119c763d8d
Megan Mattina <petersen.megan9646@gmail.com>
2021-11-02 08:23:19 -0600

    Started Python renaming tool assignment

---
+++ Python Maya Files/Python Renamer Tool Assignment.py
@@ -0,0 +1,12 @@
+def renaming():
+    
+    string  = "pShere"
+    
+    
+    
+    
+    
+#Read into replace command and how to use strings in Python 
+#Ask Clayton if any libraries need to be imported prior, so Python knows to replace what I'm asking it to. 
+    
+    
