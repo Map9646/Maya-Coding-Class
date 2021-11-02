8b849edaba47bf489fb6e5ae592775bc1e42aa74
Megan <petersen.megan9646@gmail.com>
2021-11-02 13:01:56 -0600

    Got some help with how to think out solution for the assignment

--- Python Maya Files/Python Renamer Tool Assignment.py
+++ Python Maya Files/Python Renamer Tool Assignment.py
@@ -1,6 +1,41 @@
+#Gave me some good ideas to work with. Just need to work at it and figure out how to make it all work together
+cmds.select( all=True )
 def renaming():
+   
+   cmds.rename("pSphere1", "arm_##_geo")
+   
+    ##"arm_##_geo".rfind("##")(Find Method)
     
-    string  = "pShere"
+    ##"arm_##_geo".find("##")
+    
+    #"arm_##_geo".partition("#") (Patritionm Method)
+    
+    #"arm_##_geo".count("#") (Count Method)
+    
+    #"arm_##_geo".partition("##") Searching for 2 characters and finds a number to put a number in our zfill
+    
+    #then do a zfill
+    
+    #put everything in a loop
+    
+    
+    
+    
+   
+    
+    
+   
+   
+   
+   
+
+   
+   
+   
+   
+   
+   
+   
     
     
     
