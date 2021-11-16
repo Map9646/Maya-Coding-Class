10434b4b090f07ed0f7559389ea9eb950f585f4d
Megan Petersen <petersen.megan9646@gmail.com>
2021-11-16 10:43:34 -0700

    Got Control to change colors, but it will only do red, for some reason

--- Python Maya Files/Python Assignment 3.py
+++ Python Maya Files/Python Assignment 3.py
@@ -2,10 +2,15 @@ import Maya.cmds as cmds
 
 sels = cmds.ls(sl = True)
 
-def Control()
-
     for sel in sels:
         shapes = cmds.ListRelatives(sel, children = True, shapes = True)
         
 
        
+       
+def ControlColor(newColor):
+
+   cmds.color("nurbsCircle1", rgb = (1, 0, 0))
+    
+    
+ControlColor(2)       
