7f749c3c427c76453eed017f1feee91082bdeb54
Megan Petersen <petersen.megan9646@gmail.com>
2021-11-18 11:40:55 -0700

    Almost done with code. Just needs some tweaking

--- Python Maya Files/Python Assignment 3.py
+++ Python Maya Files/Python Assignment 3.py
@@ -1,16 +1,41 @@
-import Maya.cmds as cmds
+import maya.cmds as cmds
 
-sels = cmds.ls(sl = True)
+cmds.select (all = True)
 
-    for sel in sels:
-        shapes = cmds.ListRelatives(sel, children = True, shapes = True)
+def Control()
+    
+    sels = cmds.ls(sl = True)
+
+    if sels:
+        for sel in sels: #Take this into PyCharmm on my Desktop and see what isn't working and why. 
         
-
+       
+        
+            cmds.circle (n = 'ctrl')
+        
+            cmds.group ('ctrl', n = 'ctrlGroup')
+        
+            cmds.parent ('ctrl', 'ctrlGroup')
+        
+            cmds.matchTransform ('ctrlGroup', sels)
+        
+        
+        
+        
+            #if ("_" = True):
+                
+                #cmds.rename(objName + 'Ctrl')
+            
+            
+        
+        
        
        
 def ControlColor(newColor):
+    
+  cmds.setAttr('ctrl' + 'overrideEnabled', newColor)
 
-   cmds.color("nurbsCircle1", rgb = (1, 0, 0))
+   
     
     
-ControlColor(2)       
+ControlColor(5)       
