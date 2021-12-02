793447e8abdd8b86b44896e387c9abcd48629810
Megan <petersen.megan9646@gmail.com>
2021-12-02 11:50:01 -0700

    Created Window and Added some notes

---
+++ Python Maya Files/Python Window Color Change.py
@@ -0,0 +1,4 @@
+window = cmds.window( title="Change Color", iconName='Color', widthHeight=(200, 55) )
+cmds.showWindow( window )
+#Watch videos for last class and this class, so I can make my window 
+#fix color script so it changes the color for more than one object. 
