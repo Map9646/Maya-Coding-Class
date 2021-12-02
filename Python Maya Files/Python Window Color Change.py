450c046f02da7a9b62c2d97bb8cc792e8d78feed
Megan <petersen.megan9646@gmail.com>
2021-12-02 12:41:24 -0700

    Final Save before I eat lunch and head home

--- Python Maya Files/Python Window Color Change.py
+++ Python Maya Files/Python Window Color Change.py
@@ -1,4 +1,25 @@
-window = cmds.window( title="Change Color", iconName='Color', widthHeight=(200, 55) )
-cmds.showWindow( window )
+import maya.cmds as cmds
+
+m_window = 'changeColorUI'
+
+if cmds.window(m_window, exists = True):
+    cmds.deleteUI(m_window)
+
+#puts window in a variable, so nothing gets lost in code
+
+m_window = cmds.window(m_window, title="Change Color", widthHeight=(200, 55) )
+cmds.showWindow( m_window )
 #Watch videos for last class and this class, so I can make my window 
 #fix color script so it changes the color for more than one object. 
+
+#class examples and how to make them
+
+class ToolUI():
+    def __init__(self):
+        pass
+        
+        
+
+
+def create(self):
+    #insert windows code here for creating my window. 
